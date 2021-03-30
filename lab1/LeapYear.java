b4bccd4834144e1373f4e702bb2c26c4ad9d666f
Haolien <zhlien1998@gmail.com>
2021-03-30 23:38:02 +0800

    lab1 completed

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -13,6 +13,16 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+	/** check a given year is a leap year or not*/
+	public static boolean isLeapYear(int year){
+		if(year%400 == 0){
+			return true;
+		}else if(year%4==0 && year%100!=0){
+			return true;
+		}else{
+			return false;
+		}
+	}
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
